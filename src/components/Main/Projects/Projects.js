342f8daea882fe45fabe454b626e6d159e057f66
ByronGBP <byrongbp@gmail.com>
2018-09-20 09:56:00 +0200

    features-projects animations

--- src/components/Main/Projects/Projects.js
+++ src/components/Main/Projects/Projects.js
@@ -13,6 +13,7 @@ const companies = [
     url: 'https://you.digital/system/projects/thumbs/projects_1_small_1.jpg?1522146371', 
     subtitle: 'Massimo Dutti E-commerce',
     title: 'Their premium shopping experience into the digital world',
+    key: 'first'
   },
   {
     url: './src/public/assets/images/highlighted/highlighted_02.svg', 
@@ -25,16 +26,18 @@ const companies = [
     url: 'https://you.digital/system/projects/thumbs/projects_5_small_1.jpg?1522146380', 
     subtitle: 'Mandarin Oriental',
     title: 'Hotel customers sneak into the kitchen',
+    key: 'second'
   },
   {
     url: 'https://you.digital/system/projects/thumbs/projects_14_small_2.jpg?1522146431', 
     subtitle: 'Aoo Barcelona',
     title: 'Their premium experience shopping online',
+    key: 'third'
   },
   {
     url: './src/public/assets/images/highlighted/highlighted_06.jpg', 
     subtitle: 'We are looking for you',
-    title: '"Join the team',
+    title: 'Join the team',
     isContact: true },
 ];
 
